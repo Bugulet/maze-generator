59f2786901dd6b4afdddfc13e2f13e19f6374bcb
Eusebiu Mihail Buga <eusebiu.buga@gmail.com>
2021-12-30 14:43:02 +0200

    fixed pixel renderer

--- Maze generator/Assets/ImageModificationTest.cs
+++ Maze generator/Assets/ImageModificationTest.cs
@@ -7,27 +7,47 @@ public class ImageModificationTest : MonoBehaviour
 {
     // Start is called before the first frame update
     Texture2D cTex;
-   public GameObject up,down,left,right,cube;
+    public GameObject up, down, left, right, cube;
     MazeGenerator mazeGenerator;
     void Start()
     {
-        int width = 250, height = 250;
-        cTex = new Texture2D(400,400);
+        int width = 10, height = 10;
+        cTex = new Texture2D(600,600);
         cTex.filterMode = FilterMode.Point;
-        mazeGenerator = new MazeGenerator(width,height);
+        mazeGenerator = new MazeGenerator(width, height);
         mazeGenerator.GenerateMaze();
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                MazeCell currentCell= mazeGenerator.GetCell(i, j);
-                ////cTex.SetPixel(currentCell.x, currentCell.y, currentCell.WasCellVisited() ? Color.white : Color.black);
-                //cTex.SetPixel(currentCell.x , currentCell.y, currentCell.GetWall(Direction.LEFT) ? Color.black : Color.white);
-                ////cTex.SetPixel(currentCell.x * 2 + 1, currentCell.y * 2, currentCell.GetWall(Direction.RIGHT) ? Color.black : Color.white);
-                ////cTex.SetPixel(currentCell.x * 2 , currentCell.y * 2-1, currentCell.GetWall(Direction.UP) ? Color.black : Color.white);
-                ////cTex.SetPixel(currentCell.x * 2 , currentCell.y * 2+1, currentCell.GetWall(Direction.DOWN) ? Color.black : Color.white);
-               if(currentCell.GetWall(Direction.LEFT))
-                Instantiate(left, new Vector3(i, j, 0), Quaternion.identity);
+                MazeCell currentCell = mazeGenerator.GetCell(i, j);
+
+
+                //cTex.SetPixel(currentCell.x * 2, currentCell.y * 2, Color.white);
+                if (!currentCell.GetWall(Direction.RIGHT))
+                {
+                    cTex.SetPixel(currentCell.x * 2, currentCell.y * 2, Color.white);
+                    cTex.SetPixel(currentCell.x * 2 + 1, currentCell.y * 2, Color.white);
+                }
+                if (!currentCell.GetWall(Direction.LEFT))
+                {
+                    cTex.SetPixel(currentCell.x * 2, currentCell.y * 2, Color.white);
+                    cTex.SetPixel(currentCell.x * 2 - 1, currentCell.y * 2, Color.white);
+                }
+
+                if (!currentCell.GetWall(Direction.UP))
+                {
+                    cTex.SetPixel(currentCell.x * 2, currentCell.y * 2, Color.white);
+                    cTex.SetPixel(currentCell.x * 2, currentCell.y * 2 + 1, Color.white);
+                }
+                if (!currentCell.GetWall(Direction.DOWN))
+                {
+                    cTex.SetPixel(currentCell.x * 2, currentCell.y*2, Color.white);
+                    cTex.SetPixel(currentCell.x * 2, currentCell.y*2 - 1, Color.white);
+                }
+
+                if (currentCell.GetWall(Direction.LEFT))
+                    Instantiate(left, new Vector3(i, j, 0), Quaternion.identity);
                 if (currentCell.GetWall(Direction.RIGHT))
                     Instantiate(right, new Vector3(i, j, 0), Quaternion.identity);
                 if (currentCell.GetWall(Direction.UP))
@@ -45,10 +65,10 @@ public class ImageModificationTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(cube, new Vector3(mazeGenerator.PositionsMoved[pos].x, mazeGenerator.PositionsMoved[pos].y, 0), Quaternion.identity);
-            pos++;
-        }
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    Instantiate(cube, new Vector3(mazeGenerator.PositionsMoved[pos].x, mazeGenerator.PositionsMoved[pos].y, 0), Quaternion.identity);
+        //    pos++;
+        //}
     }
 }
