6bf36162b809f74c5de39fd9cebc4c6953493bbc
Eusebiu Mihail Buga <eusebiu.buga@gmail.com>
2021-12-31 01:53:03 +0200

    black border and better scaling

--- Maze generator/Assets/MazeVisualiser.cs
+++ Maze generator/Assets/MazeVisualiser.cs
@@ -6,15 +6,17 @@ using System.Linq;
 public class MazeVisualiser : MonoBehaviour
 {
     // Start is called before the first frame update
-    Texture2D cTex;
+    Texture2D _mazeTexture;
     public GameObject up, down, left, right, cube;
     private MazeGenerator mazeGenerator;
     private float _mazeScale = 1f;
+    private Vector3 _scaleHolder;
     [SerializeField] private int _mazeWidth = 500, _mazeHeight = 500;
     [SerializeField] private Slider _widthSlider, _heightSlider, _scaleSlider;
     void Start()
     {
-        
+        //get the standard scale
+        _scaleHolder = transform.localScale;
     }
 
     void Update()
@@ -26,10 +28,11 @@ public class MazeVisualiser : MonoBehaviour
         mazeGenerator = new MazeGenerator(_mazeWidth, _mazeHeight);
 
         //create new texture, without borders
-        cTex = new Texture2D(_mazeWidth * 2 - 1, _mazeHeight * 2 - 1);
-
+        _mazeTexture = new Texture2D(_mazeWidth * 2 - 1, _mazeHeight * 2 - 1);
+        Color[] pixels = Enumerable.Repeat(Color.black, _mazeTexture.width*_mazeTexture.height).ToArray();
+        _mazeTexture.SetPixels(pixels);
         //set filtering mode to point so we dont have any smudging
-        cTex.filterMode = FilterMode.Point;
+        _mazeTexture.filterMode = FilterMode.Point;
 
         mazeGenerator.GenerateMaze();
 
@@ -39,22 +42,22 @@ public class MazeVisualiser : MonoBehaviour
             {
                 MazeCell currentCell = mazeGenerator.GetCell(i, j);
 
-                cTex.SetPixel(currentCell.x * 2, currentCell.y * 2, Color.white);
+                _mazeTexture.SetPixel(currentCell.x * 2, currentCell.y * 2, Color.white);
                 if (!currentCell.GetWall(Direction.RIGHT))
                 {
-                    cTex.SetPixel(currentCell.x * 2 + 1, currentCell.y * 2, Color.white);
+                    _mazeTexture.SetPixel(currentCell.x * 2 + 1, currentCell.y * 2, Color.white);
                 }
                 if (!currentCell.GetWall(Direction.LEFT))
                 {
-                    cTex.SetPixel(currentCell.x * 2 - 1, currentCell.y * 2, Color.white);
+                    _mazeTexture.SetPixel(currentCell.x * 2 - 1, currentCell.y * 2, Color.white);
                 }
                 if (!currentCell.GetWall(Direction.UP))
                 {
-                    cTex.SetPixel(currentCell.x * 2, currentCell.y * 2 + 1, Color.white);
+                    _mazeTexture.SetPixel(currentCell.x * 2, currentCell.y * 2 + 1, Color.white);
                 }
                 if (!currentCell.GetWall(Direction.DOWN))
                 {
-                    cTex.SetPixel(currentCell.x * 2, currentCell.y * 2 - 1, Color.white);
+                    _mazeTexture.SetPixel(currentCell.x * 2, currentCell.y * 2 - 1, Color.white);
                 }
 
                 //if (currentCell.GetWall(Direction.LEFT))
@@ -68,9 +71,9 @@ public class MazeVisualiser : MonoBehaviour
 
             }
         }
-        cTex.Apply();
-        GetComponent<RawImage>().texture = cTex;
-
+        _mazeTexture.Apply();
+        GetComponent<RawImage>().texture = _mazeTexture;
+        
         //normalize the scale of the image so its not a perfect square and dependant on the width and scale slider
         if (_mazeWidth>_mazeHeight)
         {
@@ -80,13 +83,16 @@ public class MazeVisualiser : MonoBehaviour
         {
             transform.localScale = new Vector3((float)_mazeWidth / _mazeHeight, 1, 1);
         }
-        
+
+        _scaleHolder = transform.localScale;
+        _scaleSlider.value=1;
+
     }
     public void ReadSliders()
     {
         _mazeHeight = (int)_heightSlider.value;
         _mazeWidth = (int)_widthSlider.value;
         _mazeScale = _scaleSlider.value;
-        
+        transform.localScale = _scaleHolder * _mazeScale;
     }
 }
